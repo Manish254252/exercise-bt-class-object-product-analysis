8c5272f95eb86ebf056efe98e6d6eddecea440bb
ManishSolanki.Khimjibhai <ManishSolanki.Khimjibhai@ust.com>
2024-05-11 12:15:51 +0530

    Completed Object Product Analysis

--- src/main/java/com/stackroute/oops/Product.java
+++ src/main/java/com/stackroute/oops/Product.java
@@ -5,37 +5,47 @@ package com.stackroute.oops;
     It contains parameterized constructor and Getters/Setters
  */
 public class Product {
+    private int  productCode;
+   private String name;
+   private double price;
+   private String category;
 
     public Product(int productCode, String name, double price, String category) {
+        this.productCode = productCode;
+        this.name = name;
+        this.price = price;
+        this.category = category;
     }
 
     public int getProductCode() {
-        return -1;
+        return productCode;
     }
 
     public void setProductCode(int productCode) {
-
+        this.productCode = productCode;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public double getPrice() {
-        return -1;
+        return price;
     }
 
     public void setPrice(double price) {
+        this.price = price;
     }
 
     public String getCategory() {
-        return null;
+        return category;
     }
 
     public void setCategory(String category) {
-
+        this.category = category;
     }
 }
--- src/main/java/com/stackroute/oops/ProductService.java
+++ src/main/java/com/stackroute/oops/ProductService.java
@@ -1,21 +1,51 @@
 package com.stackroute.oops;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /*
     Class for Analyzing the products present in ProductRepository
  */
 public class ProductService {
 
+
     /*
         Returns the name of the product given the productCode
      */
     public String findProductNameByCode(int productCode) {
-        return "";
+       Product[] prod =  ProductRepository.getProducts();
+       for(Product pr:prod)
+       {
+           if (pr.getProductCode()==productCode)
+           {
+               return pr.getName();
+           }
+       }
+        return null;
     }
 
     /*
         Returns the Product with maximum price in a given category
      */
     public Product findMaxPriceProductInCategory(String category) {
+        Product[] prod =  ProductRepository.getProducts();
+        double max=0;
+        for(Product pr:prod)
+        {
+            if (Objects.equals(pr.getCategory(), category))
+            {
+                max=Math.max(max, pr.getPrice());
+            }
+        }
+        for(Product pr:prod)
+        {
+            if (pr.getPrice()==max)
+            {
+                return pr;
+            }
+        }
+
         return null;
     }
 
@@ -23,6 +53,20 @@ public class ProductService {
         Returns a array of products for a given category
      */
     public Product[] getProductsByCategory(String category) {
-        return new Product[]{};
+        Product[] prod =  ProductRepository.getProducts();
+        List<Product> productList = new ArrayList<>();
+
+        for(Product pr:prod)
+        {
+            if (Objects.equals(pr.getCategory(), category))
+            {
+                productList.add(pr);
+            }
+        }
+        if(productList.isEmpty())
+            return null;
+
+
+        return productList.toArray(new Product[0]);
     }
 }
